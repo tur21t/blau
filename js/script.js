d61dba5d60df0f430e637c23fd3aab4539eb2f8b
tur <tur.ttt@gmail.com>
2024-10-22 14:24:19 +0200

    index from folder es roved to upper level to deploy in gitcloud

--- js/script.js
+++ js/script.js
@@ -14,3 +14,31 @@ document.getElementById('scroll-right').onclick = function () {
     behavior: 'smooth',
   });
 };
+
+//orm's submit action to make a request to an backend
+document.querySelector('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = {
+    name: document.getElementById('contact-form-name').value,
+    email: document.getElementById('contact-form-email').value,
+    phone: document.getElementById('contact-form-phone').value,
+    location: document.getElementById('contact-form-location').value,
+    comment: document.getElementById('comment').value,
+  };
+
+  fetch('/send-email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      alert('Message sent successfully');
+    })
+    .catch((error) => {
+      alert('Error sending message');
+    });
+});
